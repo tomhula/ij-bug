a27d88a5098fa3dbffc2d6c76c3f36a259269425
Tomasan7 <tomashula06@gmail.com>
2024-11-25 09:38:07 +0100

    Initial commit

--- src/Foo.kt
+++ src/Foo.kt
@@ -5,7 +5,7 @@ class Foo
         fun localGetBar(x: Int): Bar? = Bar(x)
 
         val bar = localGetBar(42) ?: return
-        println("X: " + bar.)
+        println("X: " + bar. /* IntelliSense here */)
     }
 
     private data class Bar(val x: Int)
